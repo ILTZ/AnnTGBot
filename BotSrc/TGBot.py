408a70bb57056ea596bd34ac7d5c70c30eee361f
ILTZ <89655084863i@gmail.com>
2024-06-09 22:53:22 +0500

    --

--- BotSrc/TGBot.py
+++ BotSrc/TGBot.py
@@ -2,12 +2,17 @@ from BotData import TG_TOKEN
 
 import telebot
 
-
 TG_ART_BOT = telebot.TeleBot(TG_TOKEN)
 
 @TG_ART_BOT.message_handler(commands=['start'])
-def StartMessage(message):
-    
+def StartMessage(message):    
+    WELCOME_MESSAGE =""
+
+    TG_ART_BOT.send_message(message.from_user.id, WELCOME_MESSAGE)    
+
+@TG_ART_BOT.message_handler(commands="regUser")
+def RegisterNewUser(message):
+
     pass
 
 
